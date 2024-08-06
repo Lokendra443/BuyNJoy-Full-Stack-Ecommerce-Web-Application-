50227a0f1eb5d1e53522a3b002f1a0562cd0cea3
Lokendra Nath <152827007+Lokendra443@users.noreply.github.com>
2024-08-06 11:10:46 +0545

    Update JwtConstant.java

--- Backend/src/main/java/com/lenncoder/config/JwtConstant.java
+++ Backend/src/main/java/com/lenncoder/config/JwtConstant.java
@@ -2,7 +2,7 @@ package com.lenncoder.config;
 
 public class JwtConstant {
 
-	public static final String SECRET_KEY = "jdoifjjfiadf9jf9ej9efhe983rjfnfdnudufh";
+	public static final String SECRET_KEY = ;
 	
 	public static final String JWT_HEADER = "Authorization";
 }
